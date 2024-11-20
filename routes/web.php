29b6406eb9a40ad54da8d2c93840e83b5acac80b
Kelvinsbs <nivlek.barcelos@gmail.com>
2024-11-20 13:16:56 -0300

    primeiro commit

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/user/{user}', function (User $user) {
+    return $user;
+});
+
+Route::prefix('usuarios')->group(function() {
+    Route::get('/', function () {
+        return 'usuarios';
+    })->name("usuario");
+    Route::get('/{id}', function () {
+        return "mostrar Detalhes";
+    })->name("usuarios.show");
+    Route::get('/{id}/tags', function () {
+        return "Tags do usuario";
+    })->name("usuarios.tags");
+    Route::get('/edit', function () {
+        return 'edit';
+    })->name("usuarios.edit");
+});
+
+Route::get('/users/{paramA}/{paramB}', function ($paramA, $paramB) { // os nomes aqui podem ser diferentes
+    return $paramA . ' - ' . $paramB;
+    // return ['Laravel' => app()->version()];
+});
+
+Route::get('/a-empresa/{string?}', function ($string = null) { // passar uma "?" no final da variavel entre "{}"  faz com que ela seja opcional
+    return $string;
+    // return ['Laravel' => app()->version()];
+});
+
+require __DIR__.'/auth.php';
