d5d6542231b31e304bc93e655d63c6e4ff53af9d
Kelvinsbs <nivlek.barcelos@gmail.com>
2024-11-25 20:52:56 -0300

    Aula 12

---
+++ database/migrations/2024_11_25_232340_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onDelete('CASCADE') // iso aqui é para quando deletar o usuario deletar os posts
+                ->onUpdate('CASCADE'); //isso aqui e para quando editar o usuario editar aqui tambem
+            $table->string('title');
+            $table->longText('body');
+            $table->string('tags')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
