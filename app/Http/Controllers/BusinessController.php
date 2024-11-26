8a731a3e1cac770709cc14e54983755ee8138e34
Kelvinsbs <nivlek.barcelos@gmail.com>
2024-11-26 13:19:36 -0300

    Aula 17 - update no BD

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 class BusinessController extends Controller
 {
     public function index() {
+
+        // FUNCOES PRINCIPAIS DO ELOQUENT ORM
         // $businesses = Business::all();
         // // dd($businesses);
 
@@ -16,13 +18,41 @@ class BusinessController extends Controller
         // $businessWhere = Business::where('name', 'Langworth, Feil and Purdy')->get();
         // $businessWhereFirst = Business::where('name', 'Langworth, Feil and Purdy')->first();
 
-        $business = Business::create([
-            'name' => 'Jon Snow',
-            'email' => 'jon@snow.com',
-            'address' => 'Rua a quadra b'
-        ]);
+        // dd($business, $businesses, $businessWhere, $businessWhereFirst);
+
+        // COMO SALVAR DADOS NO BANCO DE DADOS
+        // $business = Business::create([
+        //     'name' => 'Jon Snow',
+        //     'email' => 'jon@snow.com',
+        //     'address' => 'Rua a quadra b'
+        // ]);
+
+        // dd($business);
+
+        // COMO FAZER UM UPDATE NO BANCO DE DADOS
+        // $business = Business::find(10);
+        // $business->name = 'Kelvin';
+        // $business->email = 'Kelvin@laravel9.com';
+        // $business->address = 'quadra c rua D';
+
+        // $business->save();
+
+        // $business = Business::find(12)->update([
+        //     'name' => 'Jeniffer',
+        //     'email' => 'jeniffer@hinckel.com',
+        //     'address' => 'rua E quadra F'
+        // ]);
+
+        $input = [
+            'name' => 'Fuilanel',
+            'email' => 'fuilanel@meeeeeeu.com',
+            'address' => 'rua G quadra H'
+        ];
+
+        $business = Business::find(11);
+        $business->fill($input);
+        $business->save();
 
         dd($business);
-        // dd($business, $businesses, $businessWhere, $businessWhereFirst);
     }
 }
