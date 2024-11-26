b293ee8562f87770fe2ff421e20590e47ce963f2
Kelvinsbs <nivlek.barcelos@gmail.com>
2024-11-26 20:37:29 -0300

    Aula 19 - 1 para N

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,6 +15,15 @@ class UserController extends Controller
 
     public function show(User $user) {
 
+        $user->posts()->create([
+            'title' => 'Meu primeiro post',
+            'content' => 'Isso é um post'
+        ]);
+
+        // $user->posts()->delete();
+
+        dd($user->posts);
+
         // dd($request->header());
         return view('user', [
             'user' => $user //cada item do array vai se transformar em uma variavel la na view
--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function posts() {
+        // return $this->hasMany(Post::class, 'chave estrangeira', 'chave local');
+        return $this->hasMany(Post::class);
+    }
 }
