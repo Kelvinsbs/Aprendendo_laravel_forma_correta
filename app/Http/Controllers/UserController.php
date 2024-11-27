9971072952e484b541fdeb5c706fe50a409156f6
Kelvinsbs <nivlek.barcelos@gmail.com>
2024-11-27 12:58:31 -0300

    Aula 21 - relacao N para N

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Team;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -14,6 +15,19 @@ class UserController extends Controller
     }
 
     public function show(User $user) {
+
+        // $user->teams()->attach([1, 3]); // insere na tabela pivot
+        // $user->teams()->sync([2, 3]); // sincroniza os dados na tabela pivot (caso tenha registros repetidos)
+        // $user->teams()->syncWithoutDetaching(2); // sincroniza os dados sem remover os registros antigos
+        // $user->teams()->detach([1,3]); // remove os itens 1 e 3 da tabela pivot
+
+        // $user->load('teams');
+        // return $user;
+
+        $team = Team::find(1);
+        $team->load('users');
+
+        return $team;
 
         $user->posts()->create([
             'title' => 'Meu primeiro post',
--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
         // return $this->hasMany(Post::class, 'chave estrangeira', 'chave local');
         return $this->hasMany(Post::class);
     }
+
+    public function teams() {
+        // return $this->belongsToMany(Team::class, 'tabela', 'foreignpivotkey', 'relatedpivotkey');
+        return $this->belongsToMany(Team::class);
+    }
 }
